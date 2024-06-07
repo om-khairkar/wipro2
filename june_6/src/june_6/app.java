bb771fe07d67f82d35c583badb3d87e6dc98f3bc
om-khairkar <okhairkar6@gmail.com>
2024-06-07 10:07:14 +0530

    first commit

---
+++ june_6/src/june_6/app.java
@@ -0,0 +1,22 @@
+package june_6;
+import java.util.Scanner;
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		String b=sc.next();
+		Float c=sc.nextFloat();
+		
+		System.out.print(a);
+		System.out.print(b);
+		System.out.print(c);
+	
+	    
+		
+
+	}
+
+}
---
+++ june_6/src/june_6/app1.java
@@ -0,0 +1,10 @@
+package june_6;
+
+public class app1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
